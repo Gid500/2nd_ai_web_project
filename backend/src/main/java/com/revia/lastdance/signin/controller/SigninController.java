816ea35071e1c2063119863387679599fba144f5
Gid500 <ssqod1202@gmail.com>
2025-11-16 01:42:46 +0900

    padman ver

--- backend/src/main/java/com/revia/lastdance/signin/controller/SigninController.java
+++ backend/src/main/java/com/revia/lastdance/signin/controller/SigninController.java
@@ -4,7 +4,6 @@ import com.revia.lastdance.config.jwt.JwtUtil;
 import com.revia.lastdance.signin.dto.AuthenticationRequest;
 import com.revia.lastdance.signin.dto.AuthenticationResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
